e83c766db00bb620d8234942348576dbe3f95f7b
dfuentmo <david.fuentes.monente@gmail.com>
2025-03-05 23:01:43 +0100

    fixes

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -3,9 +3,13 @@ import NextAuth, { DefaultSession, DefaultJWT } from "next-auth";
 declare module "next-auth" {
   interface Session extends DefaultSession {
     accessToken?: string;
+    user: {
+      id: string;  // Agregar ID de usuario
+    } & DefaultSession["user"];
   }
 
   interface JWT extends DefaultJWT {
     accessToken?: string;
+    id: string;  // Agregar ID de usuario en el token
   }
 }
